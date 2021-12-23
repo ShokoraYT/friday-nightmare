5e807e7ab5670972c97218a05467263f93b7e13b
ShokoraYT <87348428+ShokoraYT@users.noreply.github.com>
2021-12-23 15:32:09 -0800

    pre-release commit

---
+++ example_mods/custom_events/TweenSpin.lua
@@ -0,0 +1,9 @@
+function onEvent(name, value1, value2)
+	
+	
+	noteTweenAngle('E',4 , 1440 + 180, 120, linear);
+	noteTweenAngle('B',5 , 1440 * -1.3, 120, linear);
+	noteTweenAngle('C',6 , 1440 + 500, 120, linear);
+	noteTweenAngle('D',7 , 1440 * -1, 120, linear);
+	
+end
